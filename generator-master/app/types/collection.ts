5c6137ad7bd0f96f14431b70016e9e31449a13d6
HelloRyanHolmes <ry.j.holmes@gmail.com>
2025-04-14 04:38:08 -0400

    Delete generator-master directory

--- generator-master/app/types/collection.ts
+++
@@ -1,9 +0,0 @@
-export interface Collection {
-  name: string;
-  AssetsUrlPath: string;
-  OrderImages: number[];
-  attributes: Array<{
-    attributeName: string;
-    maxAttributes: number;
-  }>;
-}
